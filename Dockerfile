dda7ab3e7fe8dfa46227f550b40760908c7ad3b3
Landuuu <olachao32@gmail.com>
2025-04-24 01:56:48 -0400

    integracion con render cambio variables .env

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
 FROM eclipse-temurin:21-jdk as build
-COPY . /app
 WORKDIR /app
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+COPY src ./src
 
-RUN chmod +x mvnw
 RUN ./mvnw package -DskipTests
-RUN mv -f target/*.jar app.jar
 
 FROM eclipse-temurin:21-jre
-ARG PORT
-ENV PORT=${PORT}
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
 
-COPY --from=build /app/app.jar .
-RUN useradd runtime
-USER runtime
+EXPOSE 8080
 
-ENTRYPOINT [ "java", "-Dserver.port=${PORT}", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/cynthia/apiRest/apiRest/config/EnvConfig.java
+++ src/main/java/com/cynthia/apiRest/apiRest/config/EnvConfig.java
@@ -2,18 +2,41 @@ package com.cynthia.apiRest.apiRest.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
 public class EnvConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
     @PostConstruct
     public void loadEnv() {
+
         Dotenv dotenv = Dotenv.configure().load();
 
+
         // Validar variables obligatorias
-        List<String> requiredVars = List.of("DB_URL", "DB_USERNAME", "DB_PASSWORD", "JWT_SECRET");
+        List<String> requiredVars = List.of("SPRING_DATASOURCE_URL", "SPRING_DATASOURCE_USERNAME", "SPRING_DATASOURCE_PASSWORD", "JWT_SECRET");
         for (String var : requiredVars) {
             if (dotenv.get(var) == null) {
                 throw new IllegalStateException("Falta la variable requerida en .env: " + var);
